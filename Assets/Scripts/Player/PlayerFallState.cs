9e0ee422b471b3d96b5028ae98b4f6a2688ad851
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2024-03-06 23:02:27 +0300

    Added a basic player
    -Added model and animations;
    -Added a first person camera;
    -Added a movement system.

---
+++ Assets/Scripts/Player/PlayerFallState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerFallState : PlayerBaseState
+    {
+        private readonly int _fallHash = Animator.StringToHash("Fall");
+        private const float CrossFadeDuration = 0.1f;
+        
+        public PlayerFallState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+        public override void Enter()
+        {
+            _stateMachine.velocity.y = 0f;
+            
+            _stateMachine.Animator.CrossFadeInFixedTime(_fallHash, CrossFadeDuration);
+        }
+
+        public override void Tick()
+        {
+            ApplyGravity();
+            Move();
+
+            if (_stateMachine.Controller.isGrounded)
+            {
+                _stateMachine.SwitchState(new PlayerMoveState(_stateMachine));
+            }
+        }
+
+        public override void Exit() { }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMoveState.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerMoveState : PlayerBaseState
+    {
+        private readonly int _moveSpeedHash = Animator.StringToHash("MoveSpeed");
+        private readonly int _moveBlendTreeHash = Animator.StringToHash("MoveBlendTree");
+        private const float AnimationDampTime = 0.1f;
+        private const float CrossFadeDuration = 0.1f;
+        
+        public PlayerMoveState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+        public override void Enter()
+        {
+            _stateMachine.velocity.y = Physics.gravity.y;
+            
+            _stateMachine.Animator.CrossFadeInFixedTime(_moveBlendTreeHash, CrossFadeDuration);
+
+            _stateMachine.InputReader.OnJumpPerformed += SwitchToJumpState;
+        }
+
+        public override void Tick()
+        {
+            if (!_stateMachine.Controller.isGrounded)
+            {
+                _stateMachine.SwitchState(new PlayerFallState(_stateMachine));
+            }
+            
+            CalculateMoveDirection();
+            FaceMoveDirection();
+            Move();
+            
+            _stateMachine.Animator.SetFloat(_moveSpeedHash, _stateMachine.InputReader.moveComposite.sqrMagnitude > 0f ? 1f : 0f, AnimationDampTime, Time.deltaTime);
+        }
+
+        public override void Exit()
+        {
+            _stateMachine.InputReader.OnJumpPerformed -= SwitchToJumpState;
+        }
+
+        private void SwitchToJumpState()
+        {
+            _stateMachine.SwitchState(new PlayerJumpState(_stateMachine));
+        }
+    }
+}
