6cd340b8e49711fd38bdc048d096e7b083f05f9a
Fedor Ptitsyn <fedya.ptitsyn@mail.ru>
2024-06-02 22:10:23 +0300

    Added cheer animation

---
+++ Assets/Scripts/Cheer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Cheer : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private void Start() {
+        animator.SetTrigger(Animator.StringToHash("Cheer" + Random.Range(1, 3)));   
+    }
+}
